4f562647b55884769c7b9ae14437db42a1789810
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-07 22:53:22 -0400

    Verifies available jewels
    3rd exercise - #771
    https://leetcode.com/problems/jewels-and-stones/

---
+++ stonesOrJewels.js
@@ -0,0 +1,36 @@
+/*
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.
+Each character in S is a type of stone you have.
+You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters.
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+*/
+
+//Answer//
+
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+    let A = [];
+for (let i = 0; i <J.length ; i++) {
+    A.push(S.split('').filter(x=>x===J[i]).length)
+}
+    return A.reduce((a, b) => a + b)
+};
