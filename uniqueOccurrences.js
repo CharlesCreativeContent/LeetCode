d576fad436ec9d6cc778251815486bfb4c2ae8a5
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-11 23:19:42 -0400

    Verifies if occurrences of elements are unique
    22nd exercise - #1207
    https://leetcode.com/problems/unique-number-of-occurrences/

---
+++ uniqueOccurrences.js
@@ -0,0 +1,40 @@
+/*
+Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+*/
+
+//Answer//
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    let A = []
+    let B = [...new Set (arr)]
+for (let i = 0; i <B.length ; i++) {
+A.push(arr.filter(x=>x===B[i]).length)
+}
+    console.log(typeof Array.from([...new Set(A)]))
+return A.length===[...new Set(A)].length
+};
