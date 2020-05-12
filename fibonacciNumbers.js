b2c5dc2cdf52f2439eb2eed9d60c1241e791cb26
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-12 09:55:01 -0400

    Fibonacci Sequence
    25th exercise - #509
    https://leetcode.com/problems/fibonacci-number/submissions/

---
+++ fibonacciNumbers.js
@@ -0,0 +1,33 @@
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1.
+That is,
+
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), for N > 1.
+Given N, calculate F(N).
+
+Example 1:
+
+Input: 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+*/
+
+//Answer//
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var fib = function(N) {
+let F = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040]
+return F[N]};
