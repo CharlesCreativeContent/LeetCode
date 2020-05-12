9e9fdbf3897d344c678858d8f7302a3f63bf0bb0
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-12 09:56:06 -0400

    Reverses an array
    26th exercise - #344
    https://leetcode.com/problems/reverse-string/submissions/

---
+++ reverseArray.js
@@ -0,0 +1,27 @@
+/*
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+ 
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
+
+//Answer//
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+return s.reverse()
+};
