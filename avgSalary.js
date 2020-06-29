41e3351f025213884bfb1c4ec441a17d1d055408
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-29 17:09:12 -0400

    Returns the average salary, max and min excluded
    37th exercise - leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/

---
+++ avgSalary.js
@@ -0,0 +1,38 @@
+/*
+Given an array of unique integers salary where salary[i] is the salary of the employee i.
+
+Return the average salary of employees excluding the minimum and maximum salary.
+
+ 
+
+Example 1:
+
+Input: salary = [4000,3000,1000,2000]
+Output: 2500.00000
+Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+Average salary excluding minimum and maximum salary is (2000+3000)/2= 2500
+Example 2:
+
+Input: salary = [1000,2000,3000]
+Output: 2000.00000
+Explanation: Minimum salary and maximum salary are 1000 and 3000 respectively.
+Average salary excluding minimum and maximum salary is (2000)/1= 2000
+Example 3:
+
+Input: salary = [6000,5000,4000,3000,2000,1000]
+Output: 3500.00000
+Example 4:
+
+Input: salary = [8000,9000,2000,3000,6000,1000]
+Output: 4750.00000
+*/
+
+//Answer//
+
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function(salary) {
+    return ((salary.reduce((a, b) => a + b) - (Math.max(...salary) + Math.min(...salary)))/(salary.length-2)).toFixed(5)
+};
