36acb9b0a61115094e710eacfa252c6c4fd53b02
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-08-06 22:11:19 -0400

    Update avgSalary.js

--- avgSalary.js
+++ avgSalary.js
@@ -3,7 +3,7 @@ Given an array of unique integers salary where salary[i] is the salary of the em
 
 Return the average salary of employees excluding the minimum and maximum salary.
 
- 
+  
 
 Example 1:
 
