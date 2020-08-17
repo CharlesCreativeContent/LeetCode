bbbdbec36c0dd5c17bf4de27316578e33c1974e8
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-08-16 23:31:36 -0400

    Update alternateEvenOdd.js

--- alternateEvenOdd.js
+++ alternateEvenOdd.js
@@ -1,4 +1,4 @@
-/* 
+/*
 Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
 
 Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
