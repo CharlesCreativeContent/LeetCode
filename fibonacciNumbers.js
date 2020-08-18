8d6caa844849afc727e224c5717e27fca76da45c
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-08-17 23:43:45 -0400

    Update fibonacciNumbers.js

--- fibonacciNumbers.js
+++ fibonacciNumbers.js
@@ -1,4 +1,4 @@
-/*
+/* 
 The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1.
 That is,
 
